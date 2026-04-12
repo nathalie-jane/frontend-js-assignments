e2130a6571f33aa2bfab3304c04fe4d1640ae970
nathalie-jane <nathalie.zheng@gmail.com>
2026-04-12 17:55:22 +0200

    feat(assignment-01): add JS exercise 3
    - perform basic arithmetic operations
    - store results in variables and log output

--- js/assignment-01.js
+++ js/assignment-01.js
@@ -21,3 +21,24 @@ let favoriteColor = "black";
 
 // Output personal information
 console.log(`My name is ${firstName}, I'm ${age} years old and my favorite color is ${favoriteColor}.`);
+
+/* ----------------------------------------------------
+    EXERCISE 3
+    Basic arithmetic operations
+------------------------------------------------------- */
+
+// Store numbers in two separate variables
+let number1 = 12;
+let number2 = 4;
+
+// Calculations stored in variables
+const add = number1 + number2;
+const subtract = number1 - number2;
+const multiply = number1 * number2;
+const divide = number1 / number2;
+
+// Output calculated results
+console.log(`${number1} + ${number2} = ${add}`);
+console.log(`${number1} - ${number2} = ${subtract}`);
+console.log(`${number1} * ${number2} = ${multiply}`);
+console.log(`${number1} / ${number2} = ${divide}`);
