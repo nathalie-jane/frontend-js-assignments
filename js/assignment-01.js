ec70b050eef13c3d6d98a19ac33ff683e255a380
nathalie-jane <nathalie.zheng@gmail.com>
2026-04-12 17:49:34 +0200

    feat(assignment-01): add JS exercise 2
    - store values in variables
    - log message using template literals

--- js/assignment-01.js
+++ js/assignment-01.js
@@ -8,3 +8,16 @@
 ------------------------------------------------------- */
 
 console.log("Hello, World!");
+
+/* ----------------------------------------------------
+    EXERCISE 2
+    Store and print message with template literals
+------------------------------------------------------- */
+
+// Store personal information in three separate variables
+let firstName = "Nathalie";
+let age = 34;
+let favoriteColor = "black";
+
+// Output personal information
+console.log(`My name is ${firstName}, I'm ${age} years old and my favorite color is ${favoriteColor}.`);
