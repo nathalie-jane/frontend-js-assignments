ec22c0c767b573448af2a10e1df7a551842502ca
nathalie-jane <nathalie.zheng@gmail.com>
2026-04-13 11:13:55 +0200

    feat(calculator): add DOM element references
    - add variables for inputs, buttons, and output

---
+++ js/assignment-01/calculator.js
@@ -0,0 +1,17 @@
+/* ====================================================
+    DOM SELECTORS
+    Store references to DOM elements in variables
+======================================================= */
+
+// Input fields
+const inputValue1 = document.getElementById("calculator__input--01");
+const inputValue2 = document.getElementById("calculator__input--02");
+
+// Action buttons
+const addButton = document.getElementById("btn__add");
+const subtractButton = document.getElementById("btn__subtract");
+const multiplyButton = document.getElementById("btn__multiply");
+const divideButton = document.getElementById("btn__divide");
+
+// Output field
+const outputResult = document.getElementById("calculator__output");
