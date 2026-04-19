e4aacc8d75945a77fd30262c5cd246afa14b1375
nathalie-jane <nathalie.zheng@gmail.com>
2026-04-19 16:06:59 +0200

    feat(word-counter): add text processing and word count functions
    - get input text from textarea element
    - process input and match valid words using regex
    - return total word count

--- js/assignment-02/word-counter.js
+++ js/assignment-02/word-counter.js
@@ -12,3 +12,27 @@
 
 const wordInputElement = document.getElementById("word-counter-input");
 const wordCounterValue = document.getElementById("word-counter-value");
+
+/* ----------------------------------------------------
+	INPUT HANDLING
+	- Get user input
+    - Process text and extract words using RegEx
+    - Return total word count
+------------------------------------------------------- */
+
+function getInputText(textInput) {
+	textInput = wordInputElement.value;
+	return textInput;
+}
+
+function processText(text) {
+	text = getInputText();
+	const processedText = text.trim().match(/\p{L}+(?:['-]\p{L}+)*/gu); // Valid words inludes Unicode letters, hyphens and apostrophes.
+	return processedText;
+}
+
+function extractWords(textToWords) {
+	textToWords = processText();
+	const words = textToWords.length;
+	return words;
+}
