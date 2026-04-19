dd8d9b0ac09417d8743ec937e7fa6d50da62bb1a
nathalie-jane <nathalie.zheng@gmail.com>
2026-04-19 14:00:50 +0200

    feat(assignment): update UI structure and update assignment-02 JS exercise logic
    - refactor layout, typography, and spacing
    - update HTML structure and components
    - improve input handling and validation in JS exercise for "assignment-02"

--- js/assignment-02/exercises.js
+++ js/assignment-02/exercises.js
@@ -2,54 +2,61 @@
     LESSON 02: KEYWORDS, STRING METHODS AND PARSE
 
     EXERCISE: Discount calculation
-    - Ask user for price
+    - Ask user for price that includes "$"-sign
     - Validate user input
     - Apply a 10% discount
     - Display result
 ========================================================== */
 
-let userInput = prompt("Enter a price in dollars ($): "); // Ask user for price input that inludes "$"-sign
+let userInput = prompt("Enter a price in dollars ($): ");
 
-/* ---------------------------------------
+/* -------------------------------------------------
     INPUT CHECK
-    - Prevent further code execution if
-    user cancels prompt
-    - If user input exist, continue
-    processing
------------------------------------------- */
+    - Prevent further code execution if user cancels 
+	prompt
+	- Validate input format (only one"$"-sign at the 
+	start + numbers)
+    - If user input exists and is valid, continue 
+	processing
+---------------------------------------------------- */
 
 if (userInput === null) {
 	console.log("No input provided.");
+} else if (!userInput.match(/^\$\d+$/g)) {
+	console.log(userInput);
+	console.log("Please enter a valid price.");
+	alert("Please enter a valid price.");
 } else {
 	processPriceInput();
 }
 
-/* ---------------------------------------
+/* -------------------------------------------------
     PROCESS USER INPUT
-    - Convert user input into a number
+    - Convert user input into a number (Remove
+	"$"-sign and replace comma with dot for decimals)
     - Validate user input
-    - Calculate discounted price and
-    round it to two decimal places
------------------------------------------- */
+    - Calculate discounted price and round it to two 
+	decimal places
+---------------------------------------------------- */
 
 function processPriceInput() {
-	let priceInput = userInput.replace("$", "").replace(",", "."); // Remove "$"-sign and replace comma with dot for decimals
-	let parsedPrice = parseFloat(priceInput); // Convert user input from string into number
+	let priceInput = userInput.replace("$", "").replace(",", ".");
+	let parsedPrice = parseFloat(priceInput);
 
 	// If input value is non-numeric, 0 or less than 0
 	if (Number.isNaN(parsedPrice) || parsedPrice <= 0) {
-		console.log("Please enter a valid price."); // Output in console that value is invalid
-		alert("Please enter a valid price."); // Display to user that value is invalid
-		return; // Exit function when input is invalid
+		console.log("Please enter a valid price.");
+		alert("Please enter a valid price.");
+		return;
 	}
 	// Calculate discount if input is valid
 	else {
-		const discountPercentage = 10; // 10% discount
-		const discountRate = discountPercentage / 100; // Convert 10% to decimal (0.1)
-		const discountMultiplier = 1 - discountRate; // Remaining value (0.9)
-		const finalPrice = discountMultiplier * parsedPrice; // Price after discount
+		const discountPercentage = 10;
+		const discountRate = discountPercentage / 100;
+		const discountMultiplier = 1 - discountRate;
+		const finalPrice = discountMultiplier * parsedPrice;
 
-		console.log(`The price has been reduced by 10%.\nYour final price is: $${finalPrice.toFixed(2)}`); // Output result in console
-		alert(`The price has been reduced by 10%.\nYour final price is: $${finalPrice.toFixed(2)}`); // Display result to user
+		console.log(`The price has been reduced by 10%.\nYour final price is: $${finalPrice.toFixed(2)}`);
+		alert(`The price has been reduced by 10%.\nYour final price is: $${finalPrice.toFixed(2)}`);
 	}
 }
