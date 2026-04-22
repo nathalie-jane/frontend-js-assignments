eef7d90370293168e32861210e080e87de7f36f8
nathalie-jane <nathalie.zheng@gmail.com>
2026-04-22 20:07:22 +0200

    feat(assignment-03): add username validation and error handling
    - validate username length and pattern using regex
    - display error message for invalid submission

--- js/assignment-03/signup-form.js
+++ js/assignment-03/signup-form.js
@@ -44,6 +44,7 @@ const validationPattern = {
 /* ----------------------------------------------------
 	INPUT HANDLING
 	- Get input values for username and password
+    - Display error message for invalid inputs
 ------------------------------------------------------- */
 
 function getInputValues() {
@@ -55,11 +56,46 @@ function getInputValues() {
 	return input;
 }
 
+function displayError(errorField, message) {
+	errorField.classList.add("signup-form__error--display");
+	errorField.textContent = message;
+}
+
+/* ----------------------------------------------------
+	VALIDATION: USERNAME
+    - Check length (3-20 characters)
+    - Validate RegEx pattern for username
+    - Display error message if invalid
+------------------------------------------------------- */
+
+function validateUsername(username) {
+	const usernameLength = username.length;
+	const usernamePattern = validationPattern.username.test(username);
+
+	if (usernameLength < 3 || usernameLength > 20) {
+		displayError(errorMessage.username, "Username must be between 3-20 characters.");
+		console.log("Username must be between 3-20 characters.");
+		return false;
+	}
+
+	if (!usernamePattern) {
+		displayError(errorMessage.username, "Please enter a valid username.");
+		console.log("Please enter a valid username.");
+		return false;
+	}
+
+	return true;
+}
+
 /* ----------------------------------------------------
 	EVENTS
     - Prevent default form submission
+    - Run validation on submit
 ------------------------------------------------------- */
 
 signUpForm.addEventListener("submit", function (event) {
 	event.preventDefault();
+
+	const formInputs = getInputValues();
+	const isUsernameValid = validateUsername(formInputs.username);
 });
