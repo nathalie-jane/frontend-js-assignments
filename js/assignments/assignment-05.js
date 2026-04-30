94f2f66084244c624300f3d538e36f098b4f8d75
nathalie-jane <nathalie.zheng@gmail.com>
2026-04-30 10:27:46 +0200

    feat(assignment-05): add account methods
    - add getBalance and getAccountName methods
    - include console output and return values

--- js/assignments/assignment-05.js
+++ js/assignments/assignment-05.js
@@ -1,17 +1,42 @@
 /* -----------------------------------------
 	OBJECT: ACCOUNT
 
-    - Stores account information
-    - Handles deposits and withdrawals
+    - Stores account information (name and
+    balance)
+    - Handles money transactions (deposits 
+    and withdrawals)
 --------------------------------------------- */
 
 const account = {
-    accountName = "Jane Doe",
-    balance = 10000,
-    getBalance() {},
-    deposit() {},
-    withdrawal() {},
-    getAccountName() {},
-    exitAccount() {},
-    accountError() {} 
+	accountName: "Jane Doe", // Name of account holder
+	balance: 10000, // Starting value for account balance
+
+	/* -----------------------------------------
+	    METHOD: getBalance()
+
+        - Returns current balance
+    --------------------------------------------- */
+
+	getBalance() {
+		console.log(this.balance);
+		return this.balance;
+	},
+	deposit(amount) {},
+	withdrawal(amount) {},
+
+	/* -----------------------------------------
+	    METHOD: getAccountName()
+
+        - Returns account name
+    --------------------------------------------- */
+	getAccountName() {
+		console.log(this.accountName);
+		return this.accountName;
+	},
+	exitAccount() {},
+	accountError() {},
 };
+
+// For testing
+account.getBalance();
+account.getAccountName();
