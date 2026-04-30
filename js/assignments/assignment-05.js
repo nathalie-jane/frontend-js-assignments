305bf6a4469335108ec1172bad0cb35e447c5ea6
nathalie-jane <nathalie.zheng@gmail.com>
2026-04-30 09:44:30 +0200

    feat(assignment-05): add account object
    - add account object with basic structure and empty methods

---
+++ js/assignments/assignment-05.js
@@ -0,0 +1,17 @@
+/* -----------------------------------------
+	OBJECT: ACCOUNT
+
+    - Stores account information
+    - Handles deposits and withdrawals
+--------------------------------------------- */
+
+const account = {
+    accountName = "Jane Doe",
+    balance = 10000,
+    getBalance() {},
+    deposit() {},
+    withdrawal() {},
+    getAccountName() {},
+    exitAccount() {},
+    accountError() {} 
+};
