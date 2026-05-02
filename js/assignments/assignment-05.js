d6cb84d12b602b53d7a07f422857f493a33237b0
nathalie-jane <nathalie.zheng@gmail.com>
2026-05-02 22:10:40 +0200

    feat(assignment-05): complete atm logic and connect to html
    - add menu loop, input/output handlers, and account actions
    - connect script to HTML page

--- js/assignments/assignment-05.js
+++ js/assignments/assignment-05.js
@@ -1,28 +1,52 @@
+/* =======================================================
+    LESSON 05: OBJECT AND SCOPE
+
+    ASSIGNMENT: BANK ACCOUNT
+
+	- Runs a simple ATM program
+	- Handles user input using prompt()
+	- Allows user to view account information
+	- Handles deposit and withdrawal actions
+	- Displays output using alert()
+========================================================== */
+
 /* --------------------------------------------
-	OBJECT: ACCOUNT
+	ACCOUNT OBJECT
 
-    - accountName: stores account holder name
-	- balance: stores current account balance
-    - getBalance(): returns account balance
+    - accountName
+		Stores account name
 
-	- deposit(amount): validates and adds
-	money to account
-	- withdrawal(amount): validates and
-	removes money from account
+	- balance
+		Stores current account balance
 
-	- getAccountName(): returns account name
+    - getBalance()
+		Displays and returns account balance
 
-	- exitAccount(): 
+	- deposit(amount)
+		Validates user input and adds money to
+		account. Displays and returns updated 
+		account balance
 
-	- accountError(message): handles invalid
-	input
+	- withdrawal(amount)
+		Validates user input and removes money
+		from account. Displays and returns updated 
+		account balance
+
+	- getAccountName()
+		Displays and returns account name
+
+	- exitAccount()
+		Displays message when user exits atm()
+
+	- accountError(outputMessage)
+		Displays error message for invalid input
 ------------------------------------------------ */
 
 const account = {
 	accountName: "Jane Doe",
 	balance: 10000,
 	getBalance() {
-		console.log(`Your account balance is: ${this.balance.toFixed(2)}`);
+		displayOutputMessage(`Your account balance is: ${this.balance.toFixed(2)}`);
 		return this.balance;
 	},
 	deposit(amount) {
@@ -30,7 +54,7 @@ const account = {
 			return this.accountError("Please enter a valid number.");
 		} else {
 			this.balance += amount;
-			console.log(`Transaction completed.\nYour new balance is ${this.balance.toFixed(2)}`);
+			displayOutputMessage(`Transaction completed.\nYour new balance is: ${this.balance.toFixed(2)}`);
 			return this.balance;
 		}
 	},
@@ -41,75 +65,129 @@ const account = {
 			return this.accountError("Insufficient funds.");
 		} else {
 			this.balance -= amount;
-			console.log(`Transaction completed. Your new balance is ${this.balance.toFixed(2)}`);
+			displayOutputMessage(`Transaction completed.\nYour new balance is: ${this.balance.toFixed(2)}`);
 			return this.balance;
 		}
 	},
 	getAccountName() {
-		console.log(`Your account name is: ${this.accountName}`);
+		displayOutputMessage(`Your account name is: ${this.accountName}`);
 		return this.accountName;
 	},
-	exitAccount() {},
-	accountError(message) {
-		console.log(message);
+	exitAccount() {
+		displayOutputMessage("You have exited the account.");
+	},
+	accountError(outputMessage) {
+		displayOutputMessage(outputMessage);
 		return false;
 	},
 };
 
 /* -----------------------------------------
-	FUNCTION: ATM
+	ATM MENU
 
-	- Displays menu to user
-	- Handles user selection
+	- Runs ATM menu loop
+	- Displays menu options to user
+	- Handles input for menu selection
+	- Calls account methods based on menu
+	selection
+	- Handles input for deposit and withdrawal
+	amounts
+	- Handles cancelled input
+	- Stops loop when user chooses to exit
 --------------------------------------------- */
 
 function atm() {
-	const menuOptions = prompt(
-		"********** BANK ACCOUNT **********\n\n1. Show account name\n2. Show account balance\n3. Deposit money\n4. Withdraw money\n5. Exit account\n\nSelect an option (1-5): ",
-	);
-	const userSelection = Number(menuOptions);
+	let isMenuOpen = true;
 
-	switch (userSelection) {
-		case 1: {
-			account.getAccountName();
-			break;
-		}
-		case 2: {
-			account.getBalance();
-			break;
-		}
-		case 3: {
-			const userDeposit = prompt("Enter amount to deposit: ");
+	while (isMenuOpen) {
+		const menuOptions = prompt(
+			"********** BANK ACCOUNT **********\n\n" +
+				"1. Show account name\n" +
+				"2. Show account balance\n" +
+				"3. Deposit money\n" +
+				"4. Withdraw money\n" +
+				"5. Exit account\n\n" +
+				"Select an option (1-5): ",
+		);
 
-			if (userDeposit === null) {
-				console.log("Input cancelled.");
-				return;
-			} else {
-				const amountToDeposit = Number(userDeposit);
-				account.deposit(amountToDeposit);
+		if (isInputCancelled(menuOptions)) {
+			return;
+		}
+
+		const userSelection = Number(menuOptions);
+
+		switch (userSelection) {
+			case 1: {
+				account.getAccountName();
 				break;
 			}
-		}
-		case 4: {
-			const userWithdrawal = prompt("Enter amount to withdraw: ");
-
-			if (userWithdrawal === null) {
-				console.log("Input cancelled.");
-				return;
-			} else {
-				const amountToWithdraw = Number(userWithdrawal);
-				account.withdrawal(amountToWithdraw);
+			case 2: {
+				account.getBalance();
 				break;
 			}
-		}
-		case 5: {
-			account.exitAccount();
-			break;
-		}
-		default: {
-			account.accountError("Invalid selection, please try again.");
+			case 3: {
+				const userDeposit = prompt("Enter amount to deposit: ");
+
+				if (isInputCancelled(userDeposit)) {
+					continue;
+				} else {
+					const amountToDeposit = Number(userDeposit);
+					account.deposit(amountToDeposit);
+					break;
+				}
+			}
+			case 4: {
+				const userWithdrawal = prompt("Enter amount to withdraw: ");
+
+				if (isInputCancelled(userWithdrawal)) {
+					continue;
+				} else {
+					const amountToWithdraw = Number(userWithdrawal);
+					account.withdrawal(amountToWithdraw);
+					break;
+				}
+			}
+			case 5:
+				isMenuOpen = false;
+				account.exitAccount();
+				break;
+
+			default:
+				account.accountError("Invalid selection, please try again.");
 		}
 	}
 }
+
+/* -----------------------------------------
+	OUTPUT HANDLER
+	
+	- Displays messages to user
+--------------------------------------------- */
+
+function displayOutputMessage(outputMessage) {
+	alert(outputMessage);
+	console.log(outputMessage);
+}
+
+/* -----------------------------------------
+	INPUT HANDLER
+
+	- Validates user input from prompt
+	- Returns true if cancelled, otherwise 
+	return false
+--------------------------------------------- */
+
+function isInputCancelled(userInput) {
+	if (userInput === null) {
+		console.log("Input cancelled.");
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/* -----------------------------------------
+	RUN ATM PROGRAM
+--------------------------------------------- */
 
 atm();
